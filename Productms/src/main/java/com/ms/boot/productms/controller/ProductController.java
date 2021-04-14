56113f4c78057dc73431e189b44130b380793415
Kapil Raina <kapil.raina@in.ibm.com>
2021-04-14 12:04:52 +0530

    init

--- Productms/src/main/java/com/ms/boot/productms/controller/ProductController.java
+++ Productms/src/main/java/com/ms/boot/productms/controller/ProductController.java
@@ -1,7 +1,8 @@
 package com.ms.boot.productms.controller;
 
-import java.util.List;
-
+import com.ms.boot.productms.model.Product;
+import com.ms.boot.productms.model.ProductDTO;
+import com.ms.boot.productms.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,9 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ms.boot.productms.model.Product;
-import com.ms.boot.productms.model.ProductDTO;
-import com.ms.boot.productms.service.ProductService;
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -68,15 +67,24 @@ public class ProductController {
 		}
 	}
 
-	/*
-	 * @GetMapping(path = "/product/v4/{id}", produces =
-	 * MediaType.APPLICATION_JSON_VALUE) public ResponseEntity<ProductDTO>
-	 * getProductv4(@PathVariable Integer id) { Product p =
-	 * productService.getProductById(id); if (p != null) { ProductDTO pdto =
-	 * productService.applyDiscountV4(p); return new
-	 * ResponseEntity<ProductDTO>(pdto, HttpStatus.FOUND); } else { return new
-	 * ResponseEntity<ProductDTO>(HttpStatus.NOT_FOUND); } }
-	 */
+
+	@GetMapping(path = "/product/v4/{id}", produces =
+			MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ProductDTO>
+	getProductv4(@PathVariable Integer id) {
+		Product p =
+				productService.getProductById(id);
+		if (p != null) {
+			ProductDTO pdto =
+					productService.applyDiscountV4(p);
+			return new
+					ResponseEntity<ProductDTO>(pdto, HttpStatus.FOUND);
+		} else {
+			return new
+					ResponseEntity<ProductDTO>(HttpStatus.NOT_FOUND);
+		}
+	}
+
 	@GetMapping(path = "/product/v5/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ProductDTO> getProductv5(@PathVariable Integer id) {
 		Product p = productService.getProductById(id);
--- Productms/src/main/java/com/ms/boot/productms/service/ProductService.java
+++ Productms/src/main/java/com/ms/boot/productms/service/ProductService.java
@@ -1,11 +1,19 @@
 package com.ms.boot.productms.service;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.ms.boot.productms.model.DiscountRequest;
+import com.ms.boot.productms.model.DiscountResponse;
+import com.ms.boot.productms.model.Product;
+import com.ms.boot.productms.model.ProductDTO;
+import com.ms.boot.productms.repo.ProductRepository;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
@@ -16,140 +24,160 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import com.ms.boot.productms.model.DiscountRequest;
-import com.ms.boot.productms.model.DiscountResponse;
-import com.ms.boot.productms.model.Product;
-import com.ms.boot.productms.model.ProductDTO;
-import com.ms.boot.productms.repo.ProductRepository;
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ProductService {
-	
-	private Logger logger;
 
-	@Autowired
-	ProductRepository repo;
+    @Autowired
+    ProductRepository repo;
+    @Autowired
+    DiscoveryClient discoveryClient;
+    @Autowired
+    LoadBalancerClient lbClient;
+    @Autowired
+    DiscountServiceProxyInterface discountProxy;
+    @Autowired
+    RestTemplate restTemplate;
+    private Logger logger;
+    @Autowired
+    private CircuitBreakerFactory cbFactory;
 
-	@Autowired
-	DiscoveryClient discoveryClient;
+    @Bean
+    @LoadBalanced
+    RestTemplate loadBalancedrestTemplate() {
+        return new RestTemplate();
+    }
 
-	@Autowired
-	LoadBalancerClient lbClient;
+    @Bean
+    Customizer<Resilience4JCircuitBreakerFactory> defaultR4JCustomizer() {
+        return factory -> factory.configureDefault(
+                id -> new Resilience4JConfigBuilder(id).
+                        timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build())
+                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults()).build()
 
-	@Autowired
-	DiscountServiceProxyInterface discountProxy;
+        );
+    }
 
-	@Bean
-	@LoadBalanced
-	RestTemplate loadBalancedrestTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    Customizer<Resilience4JCircuitBreakerFactory> discoutmsR4JCustomizer() {
 
-	@Autowired
-	RestTemplate restTemplate;
+        return
+                factory -> factory.configure(builder -> builder.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults()).
+                        timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(2)).build()), "V4-CircitBreaker");
 
-	public List<Product> getAllProducts() {
-		return repo.findAll();
-	}
+    }
 
-	public Product getProductById(Integer id) {
-		Optional<Product> op = repo.findById(id);
-		if (op.isPresent())
-			return op.get();
-		else
-			return null;
-	}
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
 
-	public ProductDTO applyDiscountV1(Product p) {
+    public Product getProductById(Integer id) {
+        Optional<Product> op = repo.findById(id);
+        if (op.isPresent())
+            return op.get();
+        else
+            return null;
+    }
 
-		DiscountRequest drequest = createDiscountRequest(p);
-		List<ServiceInstance> instances = discoveryClient.getInstances("discountms");
-		for (ServiceInstance instance : instances) {
-			System.out.println(instance.getHost() + ":" + instance.getPort());
-		}
-		ServiceInstance instance = instances.get(0);
-		String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/caldisc";
+    public ProductDTO applyDiscountV1(Product p) {
 
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<DiscountRequest> dre = new HttpEntity<DiscountRequest>(drequest);
+        DiscountRequest drequest = createDiscountRequest(p);
+        List<ServiceInstance> instances = discoveryClient.getInstances("discountms");
+        for (ServiceInstance instance : instances) {
+            System.out.println(instance.getHost() + ":" + instance.getPort());
+        }
+        ServiceInstance instance = instances.get(0);
+        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/caldisc";
 
-		ResponseEntity<DiscountResponse> dResponseEntity = restTemplate.exchange(url, HttpMethod.POST, dre,
-				DiscountResponse.class);
-		return ceateProductResponseDTO(dResponseEntity.getBody(), p);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<DiscountRequest> dre = new HttpEntity<DiscountRequest>(drequest);
 
-	}
+        ResponseEntity<DiscountResponse> dResponseEntity = restTemplate.exchange(url, HttpMethod.POST, dre,
+                DiscountResponse.class);
+        return ceateProductResponseDTO(dResponseEntity.getBody(), p);
 
-	public ProductDTO applyDiscountV2(Product p) {
-		ServiceInstance instance = lbClient.choose("discountms");
-		DiscountRequest drequest = createDiscountRequest(p);
-		String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/caldisc";
+    }
 
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<DiscountRequest> dre = new HttpEntity<DiscountRequest>(drequest);
+    public ProductDTO applyDiscountV2(Product p) {
+        ServiceInstance instance = lbClient.choose("discountms");
+        DiscountRequest drequest = createDiscountRequest(p);
+        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/caldisc";
 
-		ResponseEntity<DiscountResponse> dResponseEntity = restTemplate.exchange(url, HttpMethod.POST, dre,
-				DiscountResponse.class);
-		return ceateProductResponseDTO(dResponseEntity.getBody(), p);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<DiscountRequest> dre = new HttpEntity<DiscountRequest>(drequest);
 
-	}
+        ResponseEntity<DiscountResponse> dResponseEntity = restTemplate.exchange(url, HttpMethod.POST, dre,
+                DiscountResponse.class);
+        return ceateProductResponseDTO(dResponseEntity.getBody(), p);
 
-	public ProductDTO applyDiscountV3(Product p) {
+    }
 
-		DiscountRequest drequest = createDiscountRequest(p);
-		String url = "http://discountms/caldisc";
-		HttpEntity<DiscountRequest> dre = new HttpEntity<DiscountRequest>(drequest);
+    public ProductDTO applyDiscountV3(Product p) {
 
-		ResponseEntity<DiscountResponse> dResponseEntity = restTemplate.exchange(url, HttpMethod.POST, dre,
-				DiscountResponse.class);
-		return ceateProductResponseDTO(dResponseEntity.getBody(), p);
+        DiscountRequest drequest = createDiscountRequest(p);
+        String url = "http://discountms/caldisc";
+        HttpEntity<DiscountRequest> dre = new HttpEntity<DiscountRequest>(drequest);
 
-	}
+        ResponseEntity<DiscountResponse> dResponseEntity = restTemplate.exchange(url, HttpMethod.POST, dre,
+                DiscountResponse.class);
+        return ceateProductResponseDTO(dResponseEntity.getBody(), p);
 
-	/*
-	 * @HystrixCommand(fallbackMethod = "discountFallback", commandKey =
-	 * "V4-CircitBreaker") public ProductDTO applyDiscountV4(Product p) {
-	 * 
-	 * DiscountRequest drequest = createDiscountRequest(p); String url =
-	 * "http://discountms/caldisc"; HttpEntity<DiscountRequest> dre = new
-	 * HttpEntity<DiscountRequest>(drequest);
-	 * 
-	 * ResponseEntity<DiscountResponse> dResponseEntity = restTemplate.exchange(url,
-	 * HttpMethod.POST, dre, DiscountResponse.class); return
-	 * ceateProductResponseDTO(dResponseEntity.getBody(), p);
-	 * 
-	 * }
-	 */
+    }
 
-	public ProductDTO applyDiscountV5(Product p) {
 
-		//logger.info("Applying Discount for :" + p);
-		DiscountRequest drequest = createDiscountRequest(p);
-		return ceateProductResponseDTO(discountProxy.calculateDiscount(drequest), p);
+    public ProductDTO applyDiscountV4(Product p) {
+        return cbFactory.create("V4-CircitBreaker").run(() -> {
+            DiscountRequest drequest = createDiscountRequest(p);
+            String url =
+                    "http://discountms/caldisc";
+            HttpEntity<DiscountRequest> dre = new
+                    HttpEntity<DiscountRequest>(drequest);
 
-	}
+            ResponseEntity<DiscountResponse> dResponseEntity = restTemplate.exchange(url,
+                    HttpMethod.POST, dre, DiscountResponse.class);
 
-	public ProductDTO discountFallback(Product p) {
-		DiscountResponse discountResponse = new DiscountResponse(p.getCategory(), p.getMrp(), p.getMrp(), 0.0, 0.0);
-		return ceateProductResponseDTO(discountResponse, p);
-	}
+            return ceateProductResponseDTO(dResponseEntity.getBody(), p);
+        }, throwable -> {
+            return discountFallback(p);
+        });
 
-	private DiscountRequest createDiscountRequest(Product p) {
-		return new DiscountRequest(p.getCategory(), p.getMrp());
-	}
 
-	private ProductDTO ceateProductResponseDTO(DiscountResponse discountResponse, Product p) {
-		ProductDTO pdto = new ProductDTO();
-		pdto.setCategory(p.getCategory());
-		pdto.setDrp(discountResponse.getDrp());
-		pdto.setFixedCategoryDiscount(discountResponse.getFixedCategoryDiscount());
-		pdto.setOnSpotDiscount(discountResponse.getOnSpotDiscount());
-		pdto.setId(p.getId());
-		pdto.setMrp(p.getMrp());
-		pdto.setName(p.getName());
-		pdto.setShortDescription(p.getShortDescription());
-		pdto.setTags(p.getTags());
+    }
 
-		return pdto;
-	}
+
+    public ProductDTO applyDiscountV5(Product p) {
+
+        //logger.info("Applying Discount for :" + p);
+        DiscountRequest drequest = createDiscountRequest(p);
+        return ceateProductResponseDTO(discountProxy.calculateDiscount(drequest), p);
+
+    }
+
+    public ProductDTO discountFallback(Product p) {
+        DiscountResponse discountResponse = new DiscountResponse(p.getCategory(), p.getMrp(), p.getMrp(), 0.0, 0.0);
+        return ceateProductResponseDTO(discountResponse, p);
+    }
+
+    private DiscountRequest createDiscountRequest(Product p) {
+        return new DiscountRequest(p.getCategory(), p.getMrp());
+    }
+
+    private ProductDTO ceateProductResponseDTO(DiscountResponse discountResponse, Product p) {
+        ProductDTO pdto = new ProductDTO();
+        pdto.setCategory(p.getCategory());
+        pdto.setDrp(discountResponse.getDrp());
+        pdto.setFixedCategoryDiscount(discountResponse.getFixedCategoryDiscount());
+        pdto.setOnSpotDiscount(discountResponse.getOnSpotDiscount());
+        pdto.setId(p.getId());
+        pdto.setMrp(p.getMrp());
+        pdto.setName(p.getName());
+        pdto.setShortDescription(p.getShortDescription());
+        pdto.setTags(p.getTags());
+
+        return pdto;
+    }
 
 }
